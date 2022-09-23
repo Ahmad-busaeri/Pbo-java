8776ad34f5102808c013edff3f0d06c447a7139c
eyO18 <106310176+Ahmad-busaeri@users.noreply.github.com>
2022-09-23 15:00:06 +0700

    Add files via upload

---
+++ LearJava1.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+class LearJava1{
+    public static void main(String[] args) {
+        System.out.println("Belajar JAVA");
+        System.out.println("Sangat mudah sekali");
+        System.out.println("dan sangat menyenangkan");
+
+        
+
+        String input=JOptionPane.showInputDialog( "Anda Sedang belajar apa?");
+        
+        JOptionPane.showMessageDialog(null, "Belajar "+input+ " sangat mudah");
+    }
+}
