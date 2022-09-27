0a368a733f43f689c9a1c4148abeb825488adb46
eyO18 <106310176+Ahmad-busaeri@users.noreply.github.com>
2022-09-27 16:42:15 +0700

    Update LearJava1.java

--- LearJava1.java
+++ LearJava1.java
@@ -2,9 +2,9 @@ import javax.swing.*;
 
 class LearJava1{
     public static void main(String[] args) {
-        System.out.println("Belajar JAVA");
-        System.out.println("Sangat mudah sekali");
-        System.out.println("dan sangat menyenangkan");
+        System.out.println("\"Belajar JAVA\"");
+        System.out.println("\"Sangat mudah sekali\"");
+        System.out.println("\"dan sangat menyenangkan\"");
 
         
 
